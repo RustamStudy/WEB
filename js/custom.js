70174dc75b0be5faeaf9c5f1bc1e1773e1150567
RustamStudy <rustamm85@outlook.com>
2020-09-14 18:30:28 +0300

    тз19 доделаны информации от API

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,6 @@
-const initOwl = function(){
-  $(".owl-carousel").owlCarousel({
-    nav:true
-  });
-}
+//const initOwl 
 
-$(document).ready(initOwl);
+ //;
 
 const genre_1 = 'мультфильм';
 const genre_2 = 'фэнтези';
@@ -17,6 +13,7 @@ const genre_8 = 'криминал';
 const genre_9 = 'приключения';
 const genre_10 = 'семейный';
 
+let countEnterFilm = 0
 
 const films = [
   {
@@ -69,13 +66,13 @@ const idFilmsName4 = document.getElementById('idFilmsName4');
 idFilmsName4.innerHTML = films[3].name;
 const idjenre4 = document.getElementById('idjenre4');
 idjenre4.innerHTML = films[3].genre;*/
-if(document.getElementsByClassName('movie-list__table')[0])
-for (var i = 0; i<4; i++)
-{
-  document.getElementById('idFilmsStart'+(i+1)).innerHTML = films[i].start;
-  document.getElementById('idFilmsName'+(i+1)).innerHTML = films[i].name;
-  document.getElementById('idjenre'+(i+1)).innerHTML = films[i].genre;
-}
+// if(document.getElementsByClassName('movie-list__table')[0])
+// for (var i = 0; i<4; i++)
+// {
+//   document.getElementById('idFilmsStart'+(i+1)).innerHTML = films[i].start;
+//   document.getElementById('idFilmsName'+(i+1)).innerHTML = films[i].name;
+//   document.getElementById('idjenre'+(i+1)).innerHTML = films[i].genre;
+// }
 
 
 const form = document.getElementById('form')
@@ -102,3 +99,244 @@ const form = document.getElementById('form')
 //   event.preventDefault();
 //   console.log('work')
 // })
+
+const closePopup = document.getElementById('popup_close');
+const closeOpen = document.getElementById('popup_open');
+const sendForm = document.getElementById('submit');
+
+const popup = document.getElementById('popup');
+
+closePopup.onclick = function(event){
+    event.preventDefault();
+    popup.classList.add('hidden');
+}
+closeOpen.onclick = function(event){
+  event.preventDefault();
+  popup.classList.remove('hidden');
+}
+
+sendForm.onclick = function(event){
+  event.preventDefault();
+
+
+  let name = document.getElementById('nameInput');
+  let select = document.getElementById('nameSelect');
+  let agree = document.getElementById('persAccess');
+  let nameParent = name.parentNode;
+  let selectParent = select.parentNode;
+  let agreeParent = agree.parentNode;
+
+  nameParent.classList.remove('error');
+  selectParent.classList.remove('error');
+  agreeParent.classList.remove('error');
+  nameParent.getElementsByClassName('popup-eror-message')[0].innerHTML = '';
+  selectParent.getElementsByClassName('popup-eror-message')[0].innerHTML  = '';
+  agreeParent.getElementsByClassName('popup-eror-message')[0].innerHTML  = '';
+
+ 
+  if(!checkInput(name.value)){    
+    nameParent.classList.add('error');
+    nameParent.getElementsByClassName('popup-eror-message')[0].innerHTML = "заполните поле 'Имя'"
+  }
+  if(select.value == 0){    
+    selectParent.classList.add('error');
+    selectParent.getElementsByClassName('popup-eror-message')[0].innerHTML = "заполните поле выберите ряд"
+  }
+  if(!agree.checked){    
+    agreeParent.classList.add('error');
+    agreeParent.getElementsByClassName('popup-eror-message')[0].innerHTML = "Подтвердите согласие"
+  }
+}
+
+function checkInput(value){
+  if (value)
+    return true;
+  else 
+    return false;
+}
+
+
+const url = 'https://kinopoiskapiunofficial.tech/api/v2.1/';
+
+const filmsArr = [
+    464963,
+    1048334,
+    535341,
+     775276,
+    706655,
+    1122114,
+    1005878,
+    1080513,
+    1379016
+]
+const getAllFilm = function(){
+  return new Promise(function(resolve, reject){
+      filmsGenerate();
+    })
+}
+const  getFilmById = function(id) {
+    return new Promise(function(resolve, reject){
+    fetch(`${url}/films/${id}`, {
+        headers: {
+            'X-API-KEY':'fc7695c5-aa8e-4a51-b2bb-85d54ed28068'
+        }
+    }).then(response => response.json()).then(resolve)
+    })
+}
+
+
+
+
+const parseFilm = function (data) {
+    data = data.data;
+    let countries = '';
+    let genres = '';
+    console.log(data)
+    data.genres.forEach(function(item){
+        genres += `${item.genre} `
+    })
+    data.countries.forEach(function(item){
+        countries += `${item.country} `
+    })
+    return{
+        name: data.nameRu,
+        country: countries,
+        genre: genres,
+        year:data.year,
+        description:data.description,
+        link:data.posterUrl,
+        img:data.webUrl
+    }
+}
+
+
+
+
+const generateFilmItem = function (name, country,genre, year, description, img, link){
+    return `
+            <div class="movie-poster__div movie-poster__div11">
+                <a class="movie-poster__div-link" target = "_blank" href="${link}">
+                    <img class="movie-poster__div-link-img" title="Игра пристолов" src="${img}" alt="img11"/>  
+                </a>
+                <div class = "movie-poster-div-obout">
+                    <a target = "_blank" href="${link}" class = "movie-poster-div-obout__header">${name}</a>
+                    <div class = "movie-poster-div-obout__line"></div>
+                    <p class = "movie-poster-div-obout__text">${description.substr(0, 100)}...</p>
+                    <div class = "movie-poster-div-obout-array">
+                        <a  class = "movie-poster-div-obout-array__link" href="">
+                            <img class = "movie-poster-div-obout-array__link-img" src="img/facebookIco.png" alt="">
+                        </a>
+                        <a  class = "movie-poster-div-obout-array__link" href="">
+                            <img class = "movie-poster-div-obout-array__link-img" src="img/twitterIco.png" alt="">
+                        </a>
+                        <a  class = "movie-poster-div-obout-array__link" href="">
+                            <img class = "movie-poster-div-obout-array__link-img" src="img/behanceIco.png" alt="">
+                        </a>
+                        <a  class = "movie-poster-div-obout-array__link" href="">
+                            <img class = "movie-poster-div-obout-array__link-img" src="img/dribbbleIco.png" alt="">
+                        </a>
+                    </div>
+                </div>
+    
+            </div>`
+
+}
+
+function getRandomNumber(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+}
+
+const generateFilmTable = function (name, country,genre, year, description, img, link){
+  //const time = getRandomNumber(0, 2) + '' 
+
+  let time1, time2, time3, time4
+  time1 = getRandomNumber (0,2)
+  time3 = getRandomNumber (0,5)
+  time4 = getRandomNumber (0,9)
+  switch (time1){
+    case 0:
+    case 1:
+      time2 = getRandomNumber (0,9)
+    default:
+      time2 = getRandomNumber (0,3)
+    }
+
+  const hours = `${time1}${time2}:${time3}${time4}`;
+
+  return `
+          <tr>
+            <td>${hours}</td>
+            <td>
+                <a target = "_blank" href="${link}">${name}</a>
+            </td>
+            <td>${genre}</td>
+            <td>
+                <img src="img/plus.png" alt="plus"/>
+            </td>  
+          </tr>
+        `
+}
+let element, table, prepareFilm;
+let filmsGenerate = new  Promise (function(resolve,reject){filmsArr.forEach(function(item){
+  let film = getFilmById(item);
+  film.then(result => {
+        prepareFilm = parseFilm(result);
+         element = generateFilmItem(prepareFilm.name, 
+                     prepareFilm.country,
+                     prepareFilm.genre,
+                     prepareFilm.year,
+                     prepareFilm.description,
+                     prepareFilm.link,
+                     prepareFilm.img  
+             )
+        table = generateFilmTable(prepareFilm.name, 
+          prepareFilm.country,
+          prepareFilm.genre,
+          prepareFilm.year,
+          prepareFilm.description,
+          prepareFilm.link,
+          prepareFilm.img
+            )
+             document.querySelector('#filmsSection').insertAdjacentHTML('beforeEnd', element);
+             document.querySelector('.movie-list__table tbody').insertAdjacentHTML('beforeEnd', table);
+             countEnterFilm++;
+     })
+ }) 
+ resolve();
+})
+
+
+// filmsGenerate.then(
+
+//       )
+
+let checkerFilm = setTimeout(function tick(){
+  if (countEnterFilm == filmsArr.length)
+  {
+    $(".owl-carousel").owlCarousel({
+      nav:true,
+      loop:true,
+      // autoplay:true, 
+        // smartSpeed:1000, 
+        // autoplayTimeout:5000, 
+        responsive:{ 
+            0:{
+                items:1
+            },
+            570:{
+                items:2
+            },
+            850:{
+                items:3
+            }
+        }
+    }),
+    console.log(123)
+  }
+  else
+  {
+    checkerFilm = setTimeout(tick,500);
+  }
+}, 500)
